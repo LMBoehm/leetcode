21fc3ae922022afd13b1c5d6c4221651d105d240
LMBoehm <linusbom@gmail.com>
2021-10-19 10:43:27 +0200

    Update first_bad_version.cpp

--- cpp/first_bad_version.cpp
+++ cpp/first_bad_version.cpp
@@ -13,3 +13,17 @@ int firstBadVersion(int n) {
         if (upper - lower < 2) return (bad) ? check : check + 1;
     }
 }
+
+// cleaner
+int binary_search(int left, int right){
+    if(left == right) return left;
+    int middle = left + (right - left)/2;
+    if (isBadVersion(middle))
+        return binary_search(left, middle);
+    else
+        return binary_search(middle + 1,right);
+}
+
+int firstBadVersion(int n) {
+    return binary_search(0, n);
+}
